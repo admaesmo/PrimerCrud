fc079f6626f2275a6f1b91780f6e27cef8bd1528
Adrian Espinosa <mauricio.espinosa@udea.edu.co>
2023-04-27 18:30:33 -0500

    Funciona! hijueputa

--- src/main/java/com/laboratorio/primercrud/Controller/toDoController.java
+++ src/main/java/com/laboratorio/primercrud/Controller/toDoController.java
@@ -1,19 +1,35 @@
 package com.laboratorio.primercrud.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.laboratorio.primercrud.Repository.toDoRepository;
+import com.laboratorio.primercrud.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 
 
     public class toDoController {
-     @GetMapping()
+    @Autowired
+    private toDoRepository todoRepository; // se crea una instancia de la clase toDoRepository para poder usar sus metodos en el controlador
+    @Autowired
+    private com.laboratorio.primercrud.Repository.toDoRepository toDoRepository;
+
+    @GetMapping("/")
         public String index() {
       //   System.out.println("Hola mundo Api Rest");
          return "Greetings from Spring Boot!";
      }
+
+     @GetMapping("/tasks")
+       public List<Task> getTasks() {
+         return todoRepository.findAll();
+             }
+    @PostMapping(value = "/savetask")
+    public String saveTask(@RequestBody Task task) {
+         toDoRepository.save(task);
+        return "saved task";
+     }
     }
-
-
