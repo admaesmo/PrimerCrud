b1f2f414d0e1cebfe46c3503b6cc5afd65448ede
Adrian Espinosa <mauricio.espinosa@udea.edu.co>
2023-04-27 15:46:16 -0500

    modificamos clase Task del modelo e interfaz toDoRepository del repositorio

---
+++ src/main/java/com/laboratorio/primercrud/Repository/toDoRepository.java
@@ -0,0 +1,6 @@
+package com.laboratorio.primercrud.Repository;
+
+import com.laboratorio.primercrud.model.Task;
+
+public interface toDoRepository extends JpaRepository<Task, Long>{
+}
--- src/main/java/com/laboratorio/primercrud/model/Task.java
+++ src/main/java/com/laboratorio/primercrud/model/Task.java
@@ -1,4 +1,42 @@
 package com.laboratorio.primercrud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.security.PrivateKey;
+
+@Entity
+
 public class Task {
+
+
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
